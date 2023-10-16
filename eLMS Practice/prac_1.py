822f8d154838c15251717fb7c90de5264dced4a7
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-16 12:39:57 +0600

    Update prac_1.py

--- eLMS Practice/prac_1.py
+++ eLMS Practice/prac_1.py
@@ -1,26 +1,16 @@
-#grade calculator
-num = float(input("Enter your marks: "))
-if num < 55 and num >=0:
-    print("Grade: F")
-elif num >= 55 and num<58:
-    print("Grade: D")
-elif num >= 58 and num<62:
-    print("Grade: D+")
-elif num >= 62 and num<66:
-    print("Grade: C-")
-elif num >= 66 and num<70:
-    print("Grade: C")
-elif num >= 70 and num<74:
-    print("Grade: C+")
-elif num >= 74 and num<78:
-    print("Grade: B-")
-elif num >= 78 and num<82:
-    print("Grade: B")
-elif num >= 82 and num<86:
-    print("Grade: B+")
-elif num >= 86 and num<90:
-    print("Grade: A-")
-elif num >= 90 and num<=100:
-    print("Grade: A")
+n1 = int(input())
+n2 = int(input())
+if n1 > n2:
+    x = n1
+    y = n2
 else:
-    print("Input Error!")
+    x = n2
+    y = n1
+sum = x + y
+print(f"Their sum is:{sum}")
+dif = x - y
+print(f"Their difference is:{dif}")
+pro = x * y
+print(f"Their product is:{pro}")
+div = x / y
+print(f"Their division is:{div}")
