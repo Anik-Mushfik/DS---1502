f852e5cd31c19467181c9d4e6b6ea282e99cc7c9
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-16 13:39:28 +0600

    lab 3

--- eLMS Practice/prac_1.py
+++ eLMS Practice/prac_1.py
@@ -1,16 +1,18 @@
-n1 = int(input())
-n2 = int(input())
-if n1 > n2:
-    x = n1
-    y = n2
+employee_name = input("Enter employee name: ")
+wrok_hour = int(input("Enter the work hours: "))
+service_time = int(input("Enter years of work: "))
+tasks_done = int(input("Enter tasks done: "))
+tasks_given = int(input("Enter tasks given: "))
+
+if (wrok_hour > 20 and service_time >= 2):
+    productivity = tasks_done / tasks_given
+    if productivity >= 0.5 and productivity <= .69:
+        print(f"{employee_name} is eligible for the Bronze bonus")
+    elif productivity >= .70 and productivity <= .89:
+        print(f"{employee_name} is eligible for the Siver bonus")
+    elif productivity >= .90 and productivity <= 1.00:
+        print(f"{employee_name} is eligible for the Gold bonus")
+    else:
+        print(f"{employee_name} is eligible for the normal bonus")
 else:
-    x = n2
-    y = n1
-sum = x + y
-print(f"Their sum is:{sum}")
-dif = x - y
-print(f"Their difference is:{dif}")
-pro = x * y
-print(f"Their product is:{pro}")
-div = x / y
-print(f"Their division is:{div}")
+    print(f"{employee_name} is not eligible for a bonus")
