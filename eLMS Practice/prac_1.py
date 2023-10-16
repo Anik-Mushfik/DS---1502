8686bb1aae068745b0d3e47d099343560423de07
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-17 00:21:47 +0600

    Update prac_1.py

---
+++ eLMS Practice/prac_1.py
@@ -0,0 +1,11 @@
+#Problem-1:
+name = input()
+age = input()
+print(f"Hello! My name is {name} and I am {age} years old.")
+
+#Problem-2:
+num = int(input())
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
