cd39472393aa243664d5b089b96a63d9f24e910c
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-17 02:18:45 +0600

    17/10/23

--- eLMS Practice/prac_1.py
+++ eLMS Practice/prac_1.py
@@ -1,11 +1,16 @@
-#Problem-1:
-name = input()
-age = input()
-print(f"Hello! My name is {name} and I am {age} years old.")
+print("Tempareture Converter")
+print("1. Celsius to Farenheit.")
+print("2. Farenheit to Celcius.")
+temp = int(input("Enter the choice of conversion: "))
 
-#Problem-2:
-num = int(input())
-if num % 2 == 0:
-    print("The number is even")
+
+if temp == 1:
+    cels = int(input("Enter the temperature in Celsius: "))
+    conv_1 = float(cels*1.8) + float(32)
+    print(f"The converted temperature is: {int(conv_1)}F")
+elif temp == 2:
+    farn = int(input("Enter the temperature in Fahrenheit: "))
+    conv_2 = float(farn-32) * float(5/9)
+    print(f"The converted temperature is: {int(conv_2)}C")
 else:
-    print("The number is odd")
+    print("Input Error!")
