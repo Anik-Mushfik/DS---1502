abc3d2d34da9b5f77bfe271e13c93cba5f103466
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-17 09:31:16 +0600

    17/10 morning

--- eLMS Practice/Conditionals Problems.py
+++ eLMS Practice/Conditionals Problems.py
@@ -82,3 +82,17 @@ elif temp == 2:
     print(f"The converted temperature is: {int(conv_2)} C")
 else:
     print("Input Error!")
+
+
+#Practice-12:
+rich  = int(input("Number of rich people: "))
+poor = int(input("Number of poor people: "))
+if rich > poor :
+    print("Do not steal.")
+if rich < poor :
+    if poor%4 == 0 and rich%3 ==0:
+        print("Steal")
+    else:
+        print("Do not steal")
+
+
--- eLMS Practice/prac_1.py
+++ eLMS Practice/prac_1.py
@@ -1,16 +1,9 @@
-print("Tempareture Converter")
-print("1. Celsius to Farenheit.")
-print("2. Farenheit to Celcius.")
-temp = int(input("Enter the choice of conversion: "))
-
-
-if temp == 1:
-    cels = int(input("Enter the temperature in Celsius: "))
-    conv_1 = float(cels*1.8) + float(32)
-    print(f"The converted temperature is: {int(conv_1)}F")
-elif temp == 2:
-    farn = int(input("Enter the temperature in Fahrenheit: "))
-    conv_2 = float(farn-32) * float(5/9)
-    print(f"The converted temperature is: {int(conv_2)}C")
-else:
-    print("Input Error!")
+rich  = int(input("Number of rich people: "))
+poor = int(input("Number of poor people: "))
+if rich > poor :
+    print("Do not steal.")
+if rich < poor :
+    if poor%4 == 0 and rich%3 ==0:
+        print("Steal.")
+    else:
+        print("Do not steal")
