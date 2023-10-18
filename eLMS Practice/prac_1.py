4825c68fafc55097abca8247f45003004942d50c
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-18 20:18:45 +0600

    Update prac_1.py

--- eLMS Practice/prac_1.py
+++
@@ -1,6 +0,0 @@
-#Problem-8:
-name = input("Enter item name: ")
-price = float(input("Enter item price: "))
-quantity = int(input("Enter item quantity: "))
-total_price = price * quantity
-print(f"The total price of {name} purchased: {total_price} BDT")
