989bfa4294f5a1cf03dd178e2a1cf3125fb002af
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-30 12:25:43 +0600

    Update Problem Set-2-(Loops).py

--- eLMS Practice/Problem Set-2-(Loops).py
+++ eLMS Practice/Problem Set-2-(Loops).py
@@ -11,3 +11,31 @@ numbers = list(range(1, n + 1))
 
 ##The * operator is used to unpack the elements of the list and pass them as separate arguments to the print() function, which then prints them with spaces in between.
 print(*numbers)
+
+
+
+#17-
+n = int(input("Enter the size of number: "))
+
+for i in range(1,n+1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+
+#18-
+n = int(input("Enter the size of number: "))
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+
+#18- 
+n = int(input("Enter the size of number: "))
+
+for i in range(1,n+1):
+    for j in range(i):
+        print(i**2, end=" ")
+    print()
