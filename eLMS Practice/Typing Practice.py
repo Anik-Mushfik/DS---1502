1c02676171bd641d051fe657b2170ec94dd1b02a
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-30 21:12:07 +0600

    Update Typing Practice.py

--- eLMS Practice/Typing Practice.py
+++ eLMS Practice/Typing Practice.py
@@ -1,7 +1,7 @@
 line = 'a quick brown fox jumps over the lazy dog'
-on = 'a quick brown fox jumps over the lazy dog'
-while on == line:
-    out = input(f'You are doing great. Try to do it faster!!!\n')
-    on = out
+first_input = input(f"Write: ['a quick brown fox jumps over the lazy dog'] to test your typing skills.\n => ")
+user_input = 'a quick brown fox jumps over the lazy dog'
+while user_input == line:
+    user_input = input(f'You are doing great. Try to do it faster!!!\n => ')
 else:
     print(f'Your typing is wrong.Try again.')
