64e4d50f4bb1ed388ace12bdd764abcb6a1c7808
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-01 02:00:01 +0600

    Update Problem Set-2-(Loops).py

--- eLMS Practice/Problem Set-2-(Loops).py
+++ eLMS Practice/Problem Set-2-(Loops).py
@@ -43,6 +43,81 @@ for x in range(0,n-1):
 print(f"Max Value: {max}")
 
 
+#5-
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if (num1 < num2):
+    x = num1; y = num2
+else:
+    y = num1; x = num2
+if x != y:
+    for i in range(x, y+1):
+        print(i**2, end=" ")
+print("END")
+
+
+#6-
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+divisors_num2 = []
+divisors_num1 = []
+for x in range(1, num1+1):
+    if (num1 % x == 0):
+        divisors_num1.append(x)
+for y in range(1, num2+1):
+    if (num2 % y == 0):
+        divisors_num2.append(y)
+gcd = 0
+for z in divisors_num1:
+    if (z in divisors_num2) and (z > gcd):
+        gcd = z
+print(f"GCD: {gcd}")
+lcm = 0
+count = 1
+while lcm !=0:
+    if (count % num1 == 0.0) and (count % num2 == 0.0):
+        lcm = count
+        print(f"LCM: {count}")
+        break
+    count += 1
+
+
+#7
+N = int(input())
+factorial = 1
+for i in range(1,N+1):
+    factorial *= i
+print(f"The factorial of {N} is {factorial}")
+
+
+#8
+N = int(input())
+if N == 1:
+    print(("Not prime"))
+else:
+    for i in range(1,N+1):
+        if i == 1 or i == N: continue
+        if N % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
+
+#or,
+N = int(input())
+for i in range(1,N+1):
+    if N == 1:
+        print(("Not prime"))
+        break
+    if i == 1 or i == N: continue
+    if N % i == 0:
+        print("Not Prime")
+        break
+else:
+    print("Prime")
+
+
+
 
 #17-
 n = int(input("Enter the size of number: "))
