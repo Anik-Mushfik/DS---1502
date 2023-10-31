286abe04b41e847956a36ce5ede09e683bd819ad
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-31 13:28:16 +0600

    31/10/23 - Online

--- eLMS Practice/Problem Set-2-(Loops).py
+++ eLMS Practice/Problem Set-2-(Loops).py
@@ -13,6 +13,36 @@ numbers = list(range(1, n + 1))
 print(*numbers)
 
 
+#3
+n = int(input())
+for x in range(1,n+1):
+    if x%2 ==0:
+        print(0,end=" ")
+    else:
+        print(1, end=" ")
+
+
+#4
+n = int(input())
+sum = 0
+for x in range(0,n):
+    user_input = float(input())
+    sum += user_input
+average = sum/n
+print(f"Average: {average}")
+print(f"Average: {sum/n}")
+
+
+#max value
+n = int(input())
+max = float(input())
+for x in range(0,n-1):
+    user_input = float(input())
+    if max<user_input:
+        max = user_input
+print(f"Max Value: {max}")
+
+
 
 #17-
 n = int(input("Enter the size of number: "))
