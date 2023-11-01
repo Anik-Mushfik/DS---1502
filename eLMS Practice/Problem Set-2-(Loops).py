b5eeda801be54bf33598e147d14ca9e5bc58b32c
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-01 14:13:53 +0600

    Update Problem Set-2-(Loops).py

--- eLMS Practice/Problem Set-2-(Loops).py
+++ eLMS Practice/Problem Set-2-(Loops).py
@@ -59,6 +59,7 @@ print("END")
 #6-
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+##GCD
 divisors_num2 = []
 divisors_num1 = []
 for x in range(1, num1+1):
@@ -72,14 +73,16 @@ for z in divisors_num1:
     if (z in divisors_num2) and (z > gcd):
         gcd = z
 print(f"GCD: {gcd}")
+##LCM
 lcm = 0
 count = 1
-while lcm !=0:
+while 1==1:
     if (count % num1 == 0.0) and (count % num2 == 0.0):
-        lcm = count
+        #lcm = count
         print(f"LCM: {count}")
         break
     count += 1
+
 
 
 #7
