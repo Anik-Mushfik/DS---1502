40765d22338af91ecc76be04c51b440af9132c9a
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-05 21:20:31 +0600

    Update prac_2.py

--- eLMS Practice/prac_2.py
+++ eLMS Practice/prac_2.py
@@ -1,7 +1,12 @@
-N = int(input())
-for i in range(1, N+2):
-    if i % 2 == 0:
-        i *= (-1)
-    if abs(i) == N+1:
-        print(i)
-        
+# How to Reverse a Number with a While Loop
+number = 67890
+reversed_number = 0
+
+while number != 0:
+    digit = number % 10
+    reversed_number = reversed_number * 10 + digit
+    number //= 10
+    
+print(reversed_number)
+
+# Returns: 9876
