3154e3e8837519517b2cb6711c4d3adbe600ea64
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-06 02:34:19 +0600

    5

--- eLMS Practice/Problem Set-2-(Loops).py
+++ eLMS Practice/Problem Set-2-(Loops).py
@@ -214,6 +214,11 @@ for number in fibonacci_series:
 
 
 #Problem - 10:
+N = int(input())
+for i in range(1,N+2):
+    if i % 2 == 0:
+         i *= (-1)
+print(i)
 
 
 #17-
--- eLMS Practice/prac_1.py
+++ eLMS Practice/prac_1.py
@@ -1,2 +1,5 @@
-
-#num = list(map(int, input().split()))
+N = int(input())
+for i in range(1,N+2):
+    if i % 2 == 0:
+         i *= (-1)
+print(i)
