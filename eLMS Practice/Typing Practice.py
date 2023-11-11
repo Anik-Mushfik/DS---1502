dc885fe7d2fe556e034826bc930b732c506c1165
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-11 14:24:00 +0600

    Update Typing Practice.py

--- eLMS Practice/Typing Practice.py
+++ eLMS Practice/Typing Practice.py
@@ -1,6 +1,7 @@
 line = 'a quick brown fox jumps over the lazy dog'
 #check = 'a,quick,brown,fox,jumps,over,the,lazy,dog'
 check = list(line.split(" "))
+#check = [i for i in line.split()]
 user_input = input(f"Write: ['a quick brown fox jumps over the lazy dog'] to test your typing skills.\n => ")
 #user_input = 'a quick brown fox jumps over the lazy dog'
 while user_input == line:
