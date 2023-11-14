734ce9c718a97dcdc3b60fd1d9ca9ecfb14aee13
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-14 13:21:29 +0600

    14/11

--- eLMS Practice/prac_1.py
+++ eLMS Practice/prac_1.py
@@ -1,13 +1,19 @@
-lst = [2,12,23,56,64,81,92]
-search = int(input())
-Found = False
-for i in lst:
-    if i == search:
-        Found = True
-        break
-if Found:
-    print("Found")
-else:
-    print("Not Found")
+lst = [1,2,2,1,3,3,5,7,1,8,8,1,2,3]
+count = 0
+for i in range(0,10):
+    for j in lst:
+        if i == j:
+            count += 1
+    if count != 0:
+        print(f"{i} has occurd for {count} times.")
+    count = 0
 
+#or,
+mylist = [1,2,2,1,3,3,5,7,1,8,8,1,2,3]
+count = [0]*10 #count = [0,0,0,0,0,0,0,0,0,0]
 
+for element in mylist:
+    count[element] = count[element] + 1
+
+for x in range(10):
+    print(f"{x} occured for {count[x]} times.")
